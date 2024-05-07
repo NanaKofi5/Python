546e23b57435ba7764429e369ee33cf3ad72f94c
NanaKofi5 <65871179+NanaKofi5@users.noreply.github.com>
2024-05-07 19:23:21 +0000

    Add files via upload

---
+++ numeric_data.py
@@ -0,0 +1,25 @@
+
+# Exercise 1: The int data type
+print("Python has three numeric types: int, float, and complex")
+myValue = 10
+print(myValue)
+print(type(myValue))
+
+# Exercise 2: The float data type
+myValue = 3.14
+print(myValue)
+print(type(myValue))
+
+# Exercise 3: The complex data type
+myValue = 5j
+print(myValue)
+print(type(myValue))
+
+# Exercise 4: The bool data type
+myValue = True
+print(myValue)
+print(type(myValue))
+
+myValue = False
+print(myValue)
+print(type(myValue))
